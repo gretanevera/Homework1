b289b58ba97f72bc1cf86ce9ddb2c40c03287c87
Greta Ivascenko <kika0125@gmail.com>
2020-11-15 13:02:41 +0200

    completed main generator

--- scripts.js
+++ scripts.js
@@ -19,50 +19,272 @@
 // plan: build one vanilla and then if has time build one on react. double the fun.
 // comments?
 
-var cars = [{
-    "id": 1,
-    "manufacturer": "Toyota",
-    "model": "Yaris",
-    "manufacturingYear": new Date('2000-01-01'),
-    "capacity": 5,
-    "dayPrice": 6,
-    "rentedFrom": new Date('2000-01-01'),
-    "rentedTo": new Date('2000-01-01'),
-    //extra stuff
-    "plateNumber": "abc123",
-    rentHistory: [
-        {
-        "dateFrom": new Date('2000-01-01'),
-        "dateTo": new Date('2000-01-01'),
-        "daysRented": 5,
-        "milesDriven": 5,
-        "dayPrice": 5,
+let car = [{
+        "id": 1,
+        "manufacturer": "Toyota",
+        "model": "Yaris",
+        "manufacturingYear": new Date('2000-01-01'),
+        "capacity": 5,
+        "dayPrice": 6,
+        "rentedFrom": new Date('2000-01-01'),
+        "rentedTo": new Date('2000-01-01'),
+        //extra stuff
+        "plateNumber": "abc123",
+        "rented": true,
+        rentHistory: [{
+                "dateFrom": new Date('2000-01-01'),
+                "dateTo": new Date('2000-01-01'),
+                "daysRented": 5,
+                "milesDriven": 5,
+                "dayPrice": 5,
+            },
+            {
+                "dateFrom": new Date('2001-01-01'),
+                "dateTo": new Date('2001-01-01'),
+                "daysRented": 5,
+                "milesDriven": 5,
+                "dayPrice": 5,
+            },
+        ]
     },
     {
-        "dateFrom": new Date('2001-01-01'),
-        "dateTo": new Date('2001-01-01'),
-        "daysRented": 5,
-        "milesDriven": 5,
-        "dayPrice": 5,
+        "id": 2,
+        "manufacturer": "lexus",
+        "model": "Yaris",
+        "manufacturingYear": new Date('2000-01-01'),
+        "capacity": 5,
+        "dayPrice": 6,
+        "rentedFrom": new Date('2000-01-01'),
+        "rentedTo": new Date('2000-01-01'),
+        //extra stuff
+        "plateNumber": "abc123",
+        "rented": false,
+        rentHistory: [{
+                "dateFrom": new Date('2000-01-01'),
+                "dateTo": new Date('2000-01-01'),
+                "daysRented": 5,
+                "milesDriven": 5,
+                "dayPrice": 5,
+            },
+            {
+                "dateFrom": new Date('2001-01-01'),
+                "dateTo": new Date('2001-01-01'),
+                "daysRented": 5,
+                "milesDriven": 5,
+                "dayPrice": 5,
+            },
+        ]
     },
-]
-}, 
+    {
+        "id": 3,
+        "manufacturer": "mazda",
+        "model": "Yaris",
+        "manufacturingYear": new Date('2000-01-01'),
+        "capacity": 5,
+        "dayPrice": 6,
+        "rentedFrom": new Date('2000-01-01'),
+        "rentedTo": new Date('2000-01-01'),
+        //extra stuff
+        "plateNumber": "abc123",
+        "rented": true,
+        rentHistory: [{
+                "dateFrom": new Date('2000-01-01'),
+                "dateTo": new Date('2000-01-01'),
+                "daysRented": 5,
+                "milesDriven": 5,
+                "dayPrice": 5,
+            },
+            {
+                "dateFrom": new Date('2001-01-01'),
+                "dateTo": new Date('2001-01-01'),
+                "daysRented": 5,
+                "milesDriven": 5,
+                "dayPrice": 5,
+
+            },
+        ]
+    },
+    {
+        "id": 4,
+        "manufacturer": "BMW",
+        "model": "Yaris",
+        "manufacturingYear": new Date('2000-01-01'),
+        "capacity": 5,
+        "dayPrice": 6,
+        "rentedFrom": new Date('2000-01-01'),
+        "rentedTo": new Date('2000-01-01'),
+        //extra stuff
+        "plateNumber": "abc123",
+        "rented": true,
+        rentHistory: [{
+                "dateFrom": new Date('2000-01-01'),
+                "dateTo": new Date('2000-01-01'),
+                "daysRented": 5,
+                "milesDriven": 5,
+                "dayPrice": 5,
+            },
+            {
+                "dateFrom": new Date('2001-01-01'),
+                "dateTo": new Date('2001-01-01'),
+                "daysRented": 5,
+                "milesDriven": 5,
+                "dayPrice": 5,
+            },
+        ]
+    },
+    {
+        "id": 5,
+        "manufacturer": "BMW",
+        "model": "Yaris",
+        "manufacturingYear": new Date('2000-01-01'),
+        "capacity": 5,
+        "dayPrice": 6,
+        "rentedFrom": new Date('2000-01-01'),
+        "rentedTo": new Date('2000-01-01'),
+        //extra stuff
+        "plateNumber": "abc123",
+        "rented": true,
+        rentHistory: [{
+                "dateFrom": new Date('2000-01-01'),
+                "dateTo": new Date('2000-01-01'),
+                "daysRented": 5,
+                "milesDriven": 5,
+                "dayPrice": 5,
+            },
+            {
+                "dateFrom": new Date('2001-01-01'),
+                "dateTo": new Date('2001-01-01'),
+                "daysRented": 5,
+                "milesDriven": 5,
+                "dayPrice": 5,
+            },
+        ]
+    },
 
 ]
-console.log(cars)
+console.log(car)
 
 
 
 
 function generateList() {
+    console.log('hello')
     //very much generate
     //pasiimti is backo (koks jis bus?) ir sumesti i lentete
+    let tbody = document.getElementById("tableBody");
+
+    let tr
+    for (let i = 0; i < car.length; i++) {
+
+        if (car[i].rented == true) {
+            tr = "<tr class='rented'>";
+        } else {
+            tr = "<tr>";
+        }
+        tr += "<td><input type='checkbox'></td>";
+        tr += " <td>" + car[i].manufacturer + "</td>";
+        tr += "  <td>" + car[i].model + "</td>";
+        tr += " <td>" + car[i].plateNumber + "</td>";
+        tr += " <td>" + car[i].dayPrice + "</td>";
+        if (car[i].rented == true) {
+            tr += "  <td>yes</td>"
+        } else {
+            tr += "  <td>no</td>"
+        }
+
+        tr += " <td><button data-target='#demo" + i + "' class='accordion-toggle' data-toggle='collapse'>details</button></td>";
+        tr += "</tr>";
+        tr += "  </tr>"
+        tr += "  <tr class='p'>"
+        tr += "  <td colspan='9' class='hiddenRow'>"
+        tr += " <div class='accordian-body collapse details-on-car' id='demo" + i + "'>"
+        tr += " <div class='detail-buttons'>"
+        tr += "<button>Edit </button>"
+        tr += "<button>History </button>"
+        tr += " <div class='panels'>"
+        tr += "<div class='left row-panel'>"
+        tr += "<p>Manufacturing Year</p>"
+        tr += "<p>Number of seats</p>"
+        tr += "</div>"
+        tr += "<div class='right row-panel'>"
+        tr += "<p>Rented Date From</th>"
+        tr += "<p>Rented Date To</p>"
+        tr += "<p>Days left on lease</p>"
+        tr += "</div>"
+        tr += "</div>"
+        tr += "<div class='button-panels-bottom panels'>"
+
+        tr += "<div class='left-bottom-row-panel'>"
+        tr += "<button>Delete </button>"
+        tr += "</div>"
+        tr += "<div class='right-bottom-row-panel'>"
+        tr += "<button>Rent/Return </button>"
+        tr += "</div>"
+        tr += " </div>"
+        tr += " </div>"
+        tr += " </td>"
+        tr += " </tr>"
+
+
+
+
+        tbody.innerHTML += tr;
+    }
 }
+// on page load?
+generateList();
+
+
+
 
 function addNewCar() {
     //ideto i cars nauja iraso bloka ir pasiusti i backa issaugoti, bet koks backas?
     //unique id: number bigger than the last, check for dublicates.
+    let tbody = document.getElementById("tableBody");
 
+    //gather info with modal probably
+
+    
+    let tr = "<tr>";
+    tr += "<td><input type='checkbox'></td>";
+    tr += " <td>" + data.manufacturer + "</td>";
+    tr += "  <td>" + data.model + "</td>";
+    tr += " <td>" + data.plateNumber + "</td>";
+    tr += " <td>" + data.dayPrice + "</td>";
+    tr += "  <td>no</td>"
+    tr += " <td><button data-target='#demo" + i + "' class='accordion-toggle' data-toggle='collapse'>details</button></td>";
+    tr += "</tr>";
+    tr += "  </tr>"
+    tr += "  <tr class='p'>"
+    tr += "  <td colspan='9' class='hiddenRow'>"
+    tr += " <div class='accordian-body collapse details-on-car' id='demo" + data.id + "'>"
+    tr += " <div class='detail-buttons'>"
+    tr += "<button>Edit </button>"
+    tr += "<button>History </button>"
+    tr += " <div class='panels'>"
+    tr += "<div class='left row-panel'>"
+    tr += "<p>Manufacturing Year</p>"
+    tr += "<p>Number of seats</p>"
+    tr += "</div>"
+    tr += "<div class='right row-panel'>"
+    tr += "<p>Rented Date From</th>"
+    tr += "<p>Rented Date To</p>"
+    tr += "<p>Days left on lease</p>"
+    tr += "</div>"
+    tr += "</div>"
+    tr += "<div class='button-panels-bottom panels'>"
+    tr += "<div class='left-bottom-row-panel'>"
+    tr += "<button>Delete </button>"
+    tr += "</div>"
+    tr += "<div class='right-bottom-row-panel'>"
+    tr += "<button>Rent/Return </button>"
+    tr += "</div>"
+    tr += " </div>"
+    tr += " </div>"
+    tr += " </td>"
+    tr += " </tr>"
+
+    tbody.innerHTML += tr;
 
 }
 
@@ -76,7 +298,7 @@ function editCarState() {
 }
 
 function addToHistory(params) {
-// add to specific part of arrObj a history block.
+    // add to specific part of arrObj a history block.
 }
 
 function showHistory(params) {
@@ -85,9 +307,10 @@ function showHistory(params) {
 
 // extra function not included in the task
 function deleteCar() {
-// delete from arrObj a specific object
+    // delete from arrObj a specific object
 }
+
 function massDelete(params) {
-    
+
 }
 //duplicate alert?
